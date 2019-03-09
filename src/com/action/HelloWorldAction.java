bd43af5e0520b3ace142bad4c784288c6462c0df
Administrator <Administrator@10.8.226.132>
2019-03-09 19:09:09 +0800

    尝试提交最初版本的贴吧web工程，使用struts框架

---
+++ src/com/action/HelloWorldAction.java
@@ -0,0 +1,13 @@
+package com.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HelloWorldAction extends ActionSupport{
+	
+	    public String execute() throws Exception{
+	        // TODO Auto-generated method stub
+	        System.out.println("Ö´ÐÐAction");
+	        return "success";
+	    }
+	}
+
